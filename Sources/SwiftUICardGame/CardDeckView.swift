f93baceea2e050e2534d22023577946012d0066e
Renaud Jenny <renaud.jenny@gmail.com>
2022-04-01 20:57:29 +0100

    feat(Card Deck View): improve the card deck view
    * Provide values to make difference between faced down card and faced up
      card as we certainly want them to be different
    * Still struggling with the proportional sizes

--- Sources/SwiftUICardGame/CardDeckView.swift
+++ Sources/SwiftUICardGame/CardDeckView.swift
@@ -1,16 +1,46 @@
 import SwiftUI
 
 public struct CardDeckView<Card: CardRepresentable>: View {
-    let cards: [Card]
+    public let cards: [Card]
+    public let facedDownProportionalSpacing: CGFloat
+    public let facedUpProportionalSpacing: CGFloat
+
+    public init(
+        cards: [Card],
+        facedDownProportionalSpacing: CGFloat = 3/100,
+        facedUpProportionalSpacing: CGFloat = 6/100
+    ) {
+        self.cards = cards
+        self.facedDownProportionalSpacing = facedDownProportionalSpacing
+        self.facedUpProportionalSpacing = facedUpProportionalSpacing
+    }
 
     public var body: some View {
         GeometryReader { geometry in
-            VStack(spacing: -geometry.size.height * 50/100) {
-                ForEach(Array(cards.enumerated().reversed()), id: \.offset) { index, card in
+            ZStack {
+                ForEach(Array(cards.enumerated()), id: \.offset) { index, card in
                     CardView(card)
+                        .frame(height: cardHeight(availabelHeight: geometry.size.height))
+                        .offset(x: 0, y: yOffset(index: index, card: card, height: geometry.size.height))
                 }
             }
         }
+        .border(Color.red)
+    }
+
+    private func cardHeight(availabelHeight: CGFloat) -> CGFloat {
+        let facedUpCount = CGFloat(cards.filter(\.isFacedUp).count)
+        let facedDownCount = CGFloat(cards.count)
+        let facedUpHeight = facedUpCount * availabelHeight * facedUpProportionalSpacing
+        let facedDownHeight = facedDownCount * availabelHeight * facedDownProportionalSpacing
+        return availabelHeight - facedUpHeight - facedDownHeight
+    }
+
+    private func yOffset(index: Int, card: Card, height: CGFloat) -> CGFloat {
+        guard index != 0 else { return 0 }
+        let previousCard = cards[index - 1]
+        let index = CGFloat(index)
+        return index * height * (previousCard.isFacedUp ? facedUpProportionalSpacing : facedDownProportionalSpacing)
     }
 }
 
@@ -24,24 +54,24 @@ struct CardDeckView_Previews: PreviewProvider {
         @State private var cards: [StandardDeckCard] = .standard52Deck { _, _ in }
             .enumerated()
             .map { index, card in
-                if index == 0 {
-                    var firstCard = card
-                    firstCard.isFacedUp = true
-                    return firstCard
+                if index == 51 || index == 50 {
+                    var lastCard = card
+                    lastCard.isFacedUp = true
+                    return lastCard
                 }
                 return card
             }
 
         var body: some View {
             VStack {
-                CardDeckView(cards: cards)
+                CardDeckView(cards: cards.suffix(5))
                     .padding()
                 Button("Next card") {
                     cards = cards
                         .enumerated()
                         .map { index, card in
                             guard index != 0 else { return nil }
-                            if index == 1 {
+                            if index == cards.count - 1 {
                                 var secondCard = card
                                 secondCard.isFacedUp = true
                                 return secondCard
